f69d0f77bf9d38296bed538fd3a9bf169ceaee0d
jaspreet-S2099 <jaspreet.s.2099@gmail.com>
2024-09-19 17:22:03 +0000

    variable learned

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 142047
+let accountEmail = "jas@gmail.com"
+var accountPassword = "123"
+accountCity = "Zira"
+
+// accountId = 2 //not allowed
+accountEmail="js@js.com"
+accountPassword="321"
+accountCity="moga"
+let accountState; //no value declared so undefined
+/*Avoid using var instead use 'let' keyword bcoz of block scope */
+
+console.log(accountId);
+console.table([accountEmail, accountPassword,accountCity,accountState]) //2nd method to use console
