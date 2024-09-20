4faa75d11c40d60b3eacab9866622cf31c3446a3
jaspreet-S2099 <jaspreet.s.2099@gmail.com>
2024-09-20 05:38:18 +0000

    datatype conversion

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "33"
+console.log(typeof(score) );
+
+let valueInNumber= Number(score) //treat or convert score always as number
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+/*
+33 = 33 a number
+33abc = NaN Not a number
+true=1
+false=0
+*/
+let isLoggedIn = 1
+let booleanisloggedin= Boolean(isLoggedIn)
+console.log(booleanisloggedin);
+
+//1= true;  0=false
+// "" = false
+//"jas" = true
+
+let somenumber=33
+let stringnum= String(somenumber)
+console.log(stringnum);
+console.log(typeof stringnum);
+
