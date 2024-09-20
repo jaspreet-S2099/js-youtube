3880a8ff870f125731d9f0ff2ae305012f89288d
jaspreet-S2099 <jaspreet.s.2099@gmail.com>
2024-09-20 05:15:11 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,25 @@
+ "use strict"; //treat all js code as newer version
+
+ //alert(3+3) // we're using nodejs, not browser
+
+ console.log(3+3); //code readability is high
+ console.log("Jas");
+
+/* 
+DATATYPES:
+number = (2)^53
+bigint
+string = ""
+boolean = true/false
+null = standalone value
+undefined = declare var but not define
+
+symbol = for uniqueness
+
+object
+*/
+ console.log(typeof null); // output's reason bcoz null is an object
+ console.log(typeof undefined);
+ console.log( typeof "Jas");
+ 
+ 
