b05e10106cba13c05214a4a37d3b28ae9ebe7008
jaspreet-S2099 <jaspreet.s.2099@gmail.com>
2024-09-20 06:08:43 +0000

    operations done

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -1,9 +1,9 @@
 let score = "33"
-console.log(typeof(score) );
+// console.log(typeof(score) );
 
 let valueInNumber= Number(score) //treat or convert score always as number
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 /*
 33 = 33 a number
@@ -12,15 +12,48 @@ true=1
 false=0
 */
 let isLoggedIn = 1
-let booleanisloggedin= Boolean(isLoggedIn)
-console.log(booleanisloggedin);
+// let booleanisloggedin= Boolean(isLoggedIn)
+// console.log(booleanisloggedin);
 
-//1= true;  0=false
-// "" = false
-//"jas" = true
+/*1= true;  0=false
+"" = false
+"jas" = true
+*/
 
 let somenumber=33
 let stringnum= String(somenumber)
-console.log(stringnum);
-console.log(typeof stringnum);
+// console.log(stringnum);
+// console.log(typeof stringnum);
+
+/***************************    operations ***************** */
+  let value=3
+  let negval= -value
+//   console.log(negval);
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3); 2 to power of 3
+console.log(2/3);
+console.log(2%3);*/
+
+let str1= "hello"
+let str2= " Jas"
+let str3= str1 + str2;
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+ "2");
+// console.log(1+"2"+2);
+// console.log(1+2+"2");
+// console.log(1+2*3);
+
+// console.log(true);
+// console.log(+true); 
+// console.log(+"");
+
+let gamecounter=100
+gamecounter++;
+console.log(gamecounter);
+
 
