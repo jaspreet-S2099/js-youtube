f7a63753ee38fac8282debccac61c053393e6511
jaspreet-S2099 <jaspreet.s.2099@gmail.com>
2024-09-20 06:45:53 +0000

    comparisons learn

---
+++ 01_basics/comparison.js
@@ -0,0 +1,27 @@
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2 != 1);
+
+console.log("2">1);      
+console.log("02">1);/**converted comparisons like these create problems */
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+/*Equality check == and comparisons like <,>,<=,>= work differently as later converts null to a number i.e, 0 */
+
+/*strict check === strictly checks datatype also while comparing */
+
+console.log("2"===2);
+
+
+
+
